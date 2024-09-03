44a45b9e0f634db8349e2cf215e316a19e9e4b6c
Aaryan Pal <76156734+aryan2001atat@users.noreply.github.com>
2024-09-03 09:38:01 +0530

    Update assignment2_password_strength.py

--- Day 1/class2/assignment2_password_strength.py
+++ Day 1/class2/assignment2_password_strength.py
@@ -1,45 +1,35 @@
 password = input("Enter your password")
 score = 0
-
+has_upper = 0
+has_lower = 0
+has_digit = 0
+has_special = 0
 specialCharacter = ['!', '@', "#", "$"]
 if len(password) >= 8:
     score += 1
-for j in l:
 
 
 for i in password:
     if i.isdigit():
-        score += 1
+        has_digit = 1
     if i in specialCharacter:
-        score += 1
+        has_special = 1
+    if i.isupper():
+        has_upper = 1
+    if i.islower():
+        has_lower = 1
 
+if has_upper and has_lower:
+    score += 1
+if has_digit:
+    score += 1
+if has_special:
+    score += 1
 
+if score == 4:
+    print("Strong password")
+elif score == 2 or score == 3:
+    print("Moderate password")
+elif score == 0 or score == 1:
+    print("Weak password")
 
-
-
-
-# password = input("Enter your password")
-# score = 0
-#l=list(password)
-# specialCharacter = ['!', '@', "#", "$"]
-# upper = [chr(num) for num in range(65, 91)]
-# lower = [chr(num) for num in range(97, 123)]
-# number = [chr(num) for num in range(48, 58)]
-# if len(password) >= 8:
-#     score = score + 1
-#
-# for i in password:
-#     if i.
-#     # if i in upper and i in lower:
-#     #     score = score+1
-#     # if i in number:
-#     #     score = score+1
-#     # if i in specialCharacter:
-#     #     score = score+1
-#
-# if score == 4:
-#     print("Strong Password")
-# elif score == 2 or score == 3:
-#     print("Moderate Password")
-# elif score == 1:
-#     print("Weak Password")
